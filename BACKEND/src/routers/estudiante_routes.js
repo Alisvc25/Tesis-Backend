55f2000761f1c68790e6310f51df603a59f71544
Alissonviracocha <alissonviracocha602@gmail.com>
2025-11-24 00:32:35 -0500

    despliegue

--- BACKEND/src/routers/estudiante_routes.js
+++ BACKEND/src/routers/estudiante_routes.js
@@ -2,7 +2,6 @@ import {Router} from 'express'
 import { loginEstudiante, listarCalificaciones } from '../controllers/estudiantes_controller.js'
 import { verificarTokenJWT } from '../middlewares/JWT.js'
 
-
 const router = Router()
 
 router.post("/login", loginEstudiante);
